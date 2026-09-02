3f8233683c66dd0892e55a225db60fbec437b000
RobertoMira <roberto.miramontes0605@gmail.com>
2026-09-02 14:46:16 -0600

    mejorado hero nosotros

--- src/pages/mains/nosotros/components/HeroNosotros/HeroNosotros.tsx
+++ src/pages/mains/nosotros/components/HeroNosotros/HeroNosotros.tsx
@@ -1,5 +1,7 @@
 import { BarraSeparadora } from "../../../../../components/ui/separadores/BarraSeparadora";
 
+import PulpoHero from "../../../../../assets/images/pulpoGrande.png";
+import BanerHero from "../../../../../assets/images/nosotrosImagenes/queHacemos.png"
 import ProyectosRealizados from '../../../../../assets/images/nosotrosImagenes/50Proyectos.png'
 import MarcasImpulsadas from '../../../../../assets/images/nosotrosImagenes/marcasImpulsadas.png'
 import PacionHacemos from '../../../../../assets/images/nosotrosImagenes/pasionHacemos.png'
@@ -20,14 +22,26 @@ export default function HeroNosotros() {
         items-center justify-center md:justify-start
       ">
         <div className="md:col-span-5 flex flex-col gap-8">
-          <h2 className="text-5xl font-extrabold">HISTORIAS QUE <span className="text-lila">CONECTAN</span> IDEAS QUE <span className="text-lila">TRANSFORMAN</span></h2>
+          <h2 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-extrabold">HISTORIAS QUE <span className="text-lila">CONECTAN</span> IDEAS QUE <span className="text-lila">TRANSFORMAN</span></h2>
+          <img className="absolute -z-10 md:w-100 lg:w-125 xl:w-150 top-0 right-2/6" src={PulpoHero} alt="Pulpo Negro Logo" />
           <BarraSeparadora />
-          <p></p>
+          <p>
+            Como un grupo creativo apasionados por contar historias poderosas. Durante un tiempo, nos Hacemos
+            enfocado en generar estrategias y experiencias, que ayuden a nuestros clientes a alcanzar los
+            objetivos de su marca.
+          </p>
+          <p>
+            Porque creemos que cada proyecto es unico y entendemos el poder de la comunicación , en Pulpo Negro
+            construimos historias de éxito.
+          </p>
           <div className="flex gap-5">
             {info.map((info) => (
               <CardInfoVectores key={info.id} imgV={info.imgV} infoMain={info.infoMain} infoAux={info.infoAux} />
             ))}
           </div>
+        </div>
+        <div className="md:col-span-7 w-full h-full flex items-start justify-center">
+            <img className="w-full rounded-2xl md:min-w-125 bg-white mix-blend-multiply " src={BanerHero} alt="Baner Estrategias" />
         </div>
       </div>
     </>
